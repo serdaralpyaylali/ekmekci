338d885a44212aa7c15f3d9255cded766fdf9160
serdaralpyaylali <serdaralpyaylali@gmail.com>
2023-12-12 11:10:59 +0300

    Fix recreating instances of object

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,23 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        Bread bread = new Bread();
+        Egg egg = new Egg();
+        Coal coal = new Coal();
+
+
         Scanner scanner = new Scanner(System.in);
-        System.out.print("*-*-*-*Ekmekçi'ye hoşgeldiniz*-*-*-*"+
-                         "\n1-Ürün stok" +
-                         "\n2-Hizmet satış" +
-                         "\n3-Müşteri hesap  sayfası" +
-                         "\n4-İşletme Defteri"+
-                         "\n5-Çıkış"+
-                         "\nLütfen bir sayfa seçin: ");
+        System.out.print("*-*-*-*Ekmekçi'ye hoşgeldiniz*-*-*-*" +
+                "\n1-Ürün stok" +
+                "\n2-Hizmet satış" +
+                "\n3-Müşteri hesap  sayfası" +
+                "\n4-İşletme Defteri" +
+                "\n5-Çıkış" +
+                "\nLütfen bir sayfa seçin: ");
         int service;
-        do{
-        service = scanner.nextInt();
+        do {
+            service = scanner.nextInt();
             System.out.println("Yönlendiriliyor...");
             try {
                 Thread.sleep(2000);
@@ -25,7 +29,7 @@ public class Main {
 
             switch (service) {
                 case 1:
-                    page1();
+                    page1(bread, egg, coal);
                     break;
                 case 2:
                     page2();
@@ -44,19 +48,17 @@ public class Main {
                     System.out.println("Geçersiz seçenek. Lütfen tekrar deneyin.");
             }
         }
-        while (service !=5);
-        }
+        while (service != 5);
+    }
 
-    private static void page1() {
-        Bread bread = new Bread();
-        Egg egg = new Egg();
-        Coal coal = new Coal();
-        System.out.println("Ürün stok sayfasına hoşgeldiniz."+
-                        "\nYapmak istedğiniz işlemi seçiniz:  " +
-                        "\n1 - Tüm ürünler için stok ve fiyat bilgisini göster" +
-                        "\n2 - Stok bilgisini güncelle" +
-                        "\n3 - Fiyat bilgisini güncelle\n"
-                );
+    private static void page1(Bread bread, Egg egg, Coal coal) {
+
+        System.out.println("Ürün stok sayfasına hoşgeldiniz." +
+                "\nYapmak istedğiniz işlemi seçiniz:  " +
+                "\n1 - Tüm ürünler için stok ve fiyat bilgisini göster" +
+                "\n2 - Stok bilgisini güncelle" +
+                "\n3 - Fiyat bilgisini güncelle\n"
+        );
         Scanner scanner = new Scanner(System.in);
         int selectOperation = scanner.nextInt();
         System.out.println("Hesaplanıyor...");
@@ -68,17 +70,17 @@ public class Main {
 
         switch (selectOperation) {
             case 1:
-                System.out.println("Ekmek stok miktarı: " + bread.getStock()+ " Ekmek fiyatı: "+ bread.getPrice() +
-                        "\nYumurta stok miktarı: " + egg.getStock()+ " Yumurta fiyatı: "+ egg.getPrice() +
-                        "\nKömür stok miktarı: " + coal.getStock()+ " Kömür fiyatı: "+ coal.getPrice());
+                System.out.println("Ekmek stok miktarı: " + bread.getStock() + " Ekmek fiyatı: " + bread.getPrice() +
+                        "\nYumurta stok miktarı: " + egg.getStock() + " Yumurta fiyatı: " + egg.getPrice() +
+                        "\nKömür stok miktarı: " + coal.getStock() + " Kömür fiyatı: " + coal.getPrice());
                 break;
             case 2:
-                System.out.println("Lütfen stok bilgisi güncellenecek ürünü seçin: "+
+                System.out.println("Lütfen stok bilgisi güncellenecek ürünü seçin: " +
                         "\n1-Ekmek \n2-Yumurta \n3-Kömür");
                 int selectOperation2 = scanner.nextInt();
                 System.out.println("Lütfen yeni değeri girin");
-                int newStockValue=scanner.nextInt();
-                switch (selectOperation2){
+                int newStockValue = scanner.nextInt();
+                switch (selectOperation2) {
                     case 1:
                         bread.setStock(newStockValue);
                         System.out.println("Stok değeri: " + bread.getStock());
@@ -94,12 +96,12 @@ public class Main {
                 }
                 break;
             case 3:
-                System.out.println("Lütfen fiyat bilgisi güncellenecek ürünü seçin: "+
+                System.out.println("Lütfen fiyat bilgisi güncellenecek ürünü seçin: " +
                         "\n1-Ekmek \n2-Yumurta \n3-Kömür");
                 int selectOperation3 = scanner.nextInt();
                 System.out.println("Lütfen yeni değeri girin");
-                int newPriceValue=scanner.nextInt();
-                switch (selectOperation3){
+                int newPriceValue = scanner.nextInt();
+                switch (selectOperation3) {
                     case 1:
                         bread.setPrice(newPriceValue);
                         System.out.println("Fiyat değeri: " + bread.getPrice());
@@ -116,6 +118,7 @@ public class Main {
                 break;
         }
     }
+
     private static void page2() {
         System.out.println("Hizmet satış sayfasına hoşgeldiniz.");
         Scanner scanner = new Scanner(System.in);
@@ -138,9 +141,11 @@ public class Main {
                 break;
         }
     }
+
     private static void page3() {
         System.out.println("Müşteri hesap  sayfasına hoşgeldiniz.");
     }
+
     private static void page4() {
         System.out.println("İşletme Defteri sayfasına hoşgeldiniz.");
     }
